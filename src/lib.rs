16c099995a97775562816aa1fb8d896928cda8b8
Brendan Biernacki <bab8@illinois.edu>
2022-05-01 14:11:27 -0500

    Functions mostly complete.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
 
 const BOARD_SIZE: usize = 8;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Board {
     pub board: [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE]
 }
@@ -13,7 +14,7 @@ pub struct Piece {
     pub location: Location,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum PieceType {
     Pawn,
     Rook,
@@ -29,7 +30,7 @@ pub enum Player {
     Player2
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct Location {
     pub row: usize,
     pub col: usize
@@ -72,8 +73,64 @@ impl Board {
         }
     }
 
-    pub fn at(&mut self, location: Location) -> Option<Piece> {
+    pub fn at_loc(&mut self, location: Location) -> Option<Piece> {
         self.board[location.row][location.col]
+    }
+
+    pub fn at(&mut self, row: usize, col: usize) -> Option<Piece> {
+        self.board[row][col]
+    }
+
+    pub fn empty_loc (&mut self, location: Location) -> bool {
+        self.at_loc(location).is_none()
+    }
+    pub fn empty(&mut self, row: usize, col: usize) -> bool {
+        self.at_loc(Location { row, col }).is_none()
+    }
+
+    pub fn to_string(&mut self) -> String {
+        let mut str: String = String::new();
+        str.push_str("-------------------------");
+        
+        let mut i = 7;
+        loop {
+            str.push_str("\n|");
+            let mut j = 0;
+            while j < 8 {
+                let entry = self.board[i][j];
+                if entry.is_none() {
+                    str.push_str("  ");
+                } else {
+                    if entry.unwrap().player == Player::Player1 {
+                        str.push_str("1");
+                    } else {
+                        str.push_str("2");
+                    }
+                    if entry.unwrap().name == PieceType::Pawn {
+                        str.push_str("P");
+                    } else if entry.unwrap().name == PieceType::Rook {
+                        str.push_str("R");
+                    } else if entry.unwrap().name == PieceType::Knight {
+                        str.push_str("N");
+                    } else if entry.unwrap().name == PieceType::Bishop {
+                        str.push_str("B");
+                    } else if entry.unwrap().name == PieceType::Queen {
+                        str.push_str("Q");
+                    } else {
+                        str.push_str("K");
+                    }
+                }
+                str.push_str("|");
+                j = j+1;
+            }
+            if i == 0 {
+                break;
+            }
+            str.push_str("\n-------------------------");
+            i = i-1;
+        }
+        str.push_str("\n-------------------------");
+        return str;
     }
 }
 
@@ -82,16 +139,210 @@ impl Piece {
         Self{name, player, location}
     }
 
-    pub fn move_to(&mut self, new_location: Location, board: &mut Board) -> Result<(), InvalidMove> {
+    pub fn move_to(&mut self, new_location: &Location, board: &mut Board) -> Result<(), InvalidMove> {
         // check if move is valid
-        if !valid_location(new_location) {
+        if !valid_location(*new_location) {
             return Err(InvalidMove::OutOfBounds)
-        } else if board.at(new_location).is_some() && board.at(new_location).unwrap().player == self.player {
+        } else if board.at_loc(*new_location).is_some() && board.at_loc(*new_location).unwrap().player == self.player {
             return Err(InvalidMove::AlreadyOccupiedBySamePlayer)
         } else {
             // make sure the new_location is in the range of the piece
+            let range = self.range(board);
+            if range.contains(new_location) {
+                if !board.empty_loc(*new_location) {
+                    // do something with board.at_loc(new_location)
+                    if self.move_to(new_location, board).is_err() {
+                        panic!();
+                    }
+                }
+            }
+            
         }
         Ok(())
+    }
+
+    pub fn range(&mut self, board: &mut Board) -> HashSet<Location> {
+        // use a set
+        // bulk of the work
+        let mut set: HashSet<Location> = std::collections::HashSet::new();
+        let row = self.location.row;
+        let col = self.location.col;
+        match self.name {
+            PieceType::Pawn => {
+                set
+            },
+            PieceType::Rook => {
+                let mut i = row;
+                let mut j = col;
+                let mut k = 0;
+                loop {
+                    if k == 0 {
+                        i = i+1;
+                    } else if k == 1 {
+                        i = i-1;
+                    } else if k == 2 {
+                        j = j+1;
+                    } else if k == 3 {
+                        j = j-1;
+                    } else {
+                        return set;
+                    }
+                    if valid_location(Location::new(i,j)) && board.at(i,j).is_none() {
+                        set.insert(Location::new(i,j));
+                    } else if valid_location(Location::new(i,j)) && board.at(i,j).unwrap().player != self.player {
+                        set.insert(Location::new(i,j));
+                        k = k+1;
+                    } else {
+                        k = k+1;
+                    }
+                }
+            },
+            PieceType::Knight => {
+                let mut k = 0;
+                loop {
+                    let r: usize;
+                    let c: usize;
+                    if k == 0 {
+                        r = row + 2;
+                        c = col + 1;
+                    } else if k == 1 {
+                        r = row + 2;
+                        c = col - 1;
+                    } else if k == 2 {
+                        r = row - 2;
+                        c = col + 1;
+                    } else if k == 3 {
+                        r = row - 2;
+                        c = col - 1;
+                    } else if k == 4 {
+                        r = row + 1;
+                        c = col + 2;
+                    } else if k == 5 {
+                        r = row + 1;
+                        c = col - 2;
+                    } else if k == 6 {
+                        r = row - 1;
+                        c = col + 2;
+                    } else if k == 7 {
+                        r = row - 1;
+                        c = col - 2;
+                    } else {
+                        return set;
+                    }
+
+                    let loc = Location::new(r,c);
+                    if valid_location(loc) && (board.at_loc(loc).is_none() || board.at_loc(loc).unwrap().player != self.player) {
+                        set.insert(loc);
+                    }
+                    k = k+1;
+                }
+            },
+            PieceType::Bishop => {
+                let mut i = row;
+                let mut j = col;
+                let mut k = 0;
+                loop {
+                    if k == 0 {
+                        i = i+1;
+                        j = j+1;
+                    } else if k == 1 {
+                        i = i+1;
+                        j = j-1;
+                    } else if k == 2 {
+                        i = i-1;
+                        j = j+1;
+                    } else if k == 3 {
+                        i = i-1;
+                        j = j-1;
+                    } else {
+                        return set;
+                    }
+                    if valid_location(Location::new(i,j)) && board.at(i,j).is_none() {
+                        set.insert(Location::new(i,j));
+                    }  else if valid_location(Location::new(i,j)) && board.at(i,j).unwrap().player != self.player {
+                        set.insert(Location::new(i,j));
+                        k = k+1;
+                    } else {
+                        k = k+1;
+                    }
+                }
+            },
+            PieceType::Queen => {
+                let mut i = row;
+                let mut j = col;
+                let mut k = 0;
+                while k < 4 {
+                    if k == 0 {
+                        i = i+1;
+                    } else if k == 1 {
+                        i = i-1;
+                    } else if k == 2 {
+                        j = j+1;
+                    } else if k == 3 {
+                        j = j-1;
+                    }
+                    if valid_location(Location::new(i,j)) && board.at(i,j).is_none() {
+                        set.insert(Location::new(i,j));
+                    } else if valid_location(Location::new(i,j)) && board.at(i,j).unwrap().player != self.player {
+                        set.insert(Location::new(i,j));
+                        k = k+1;
+                    } else {
+                        k = k+1;
+                    }
+                }
+
+                i = row;
+                j = col;
+                k = 0;
+                while k < 4 {
+                    if k == 0 {
+                        i = i+1;
+                        j = j+1;
+                    } else if k == 1 {
+                        i = i+1;
+                        j = j-1;
+                    } else if k == 2 {
+                        i = i-1;
+                        j = j+1;
+                    } else if k == 3 {
+                        i = i-1;
+                        j = j-1;
+                    }
+                    if valid_location(Location::new(i,j)) && board.at(i,j).is_none() {
+                        set.insert(Location::new(i,j));
+                    }  else if valid_location(Location::new(i,j)) && board.at(i,j).unwrap().player != self.player {
+                        set.insert(Location::new(i,j));
+                        k = k+1;
+                    } else {
+                        k = k+1;
+                    }
+                }
+
+                set
+            },
+            PieceType::King => {
+                let mut i = row - 1;
+                let mut j = col - 1;
+
+                loop {
+                    loop {
+                        if i < BOARD_SIZE && j < BOARD_SIZE && board.empty(i, j)
+                            && board.at(i,j).unwrap().player != self.player {
+                            set.insert(Location { row: i, col: j });
+                        }
+                        if j == col + 1 {
+                            break;
+                        }
+                        j += 1;
+                    }
+                    if i == row + 1 {
+                        break;
+                    }
+                    i += 1;
+                }
+                set
+            }
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-
+#[warn(unused_imports)]
+use cs128_final::{Board,Player};
 pub fn main() {
+    let mut board = Board::new();
+    let mut player = Player::Player1;
+    board.fill();
+    loop {
+        println!("{}", board.to_string());
+        if player == Player::Player1 {
+            println!("Player 1's Turn. Enter coordinates of piece as \"X0\": ");
+        }
+    }
     
 }
